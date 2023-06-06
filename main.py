5f8ffe7bbde237e6d7ee03daf20cc81b5d9eafcf
Jordan Williams <Jordanwilliams106@hotmail.com>
2023-06-06 04:25:16 -0500

    updated

--- main.py
+++ main.py
@@ -23,31 +23,6 @@ def home():
 
 
 
-""" USE FOR LATER
-@app.route('/checkout')
-def checkout():
-    # Get the user's IP address
-    user_ip = request.headers.get("X-Forwarded-For") or request.remote_addr
-    print(user_ip + " has connected to /checkout!")
-
-    # Set the user's IP address as a cookie
-    response = make_response(render_template('checkout.html', user_ip=user_ip))
-    response.set_cookie('user_ip', user_ip)
-
-    # Return the response
-    return response
-
-@app.route('/checkout/verify')
-def checkoutverify():
-    return render_template('verify.html')
-
-
-
-"""
-
-
-
-
 
 http_server = WSGIServer(('0.0.0.0', int(os.environ.get('PORT', 5000))), app)
 http_server.serve_forever()
